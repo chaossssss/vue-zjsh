c66b32c77979ec3d5ff0ee042e91625e717737de
zhouhao <2153229687@qq.com>
2017-03-03 11:33:02 +0800

    商户详情

--- wapapp/components/business/Business_detail.vue
+++ wapapp/components/business/Business_detail.vue
@@ -1,83 +1,67 @@
 <template>
 <div>
-	<header style="background-image: url(http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg);">
+	<header :style="{'background-image':'url('+us.Photo+')'}" style="position:fixed;top:0;left:0;z-index:3;background-color:#fff;">
 		<div class="shade"></div>
 		<div class="header-cont">
-			<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="" class="head-img">
+			<img :src="us.Photo" alt="" class="head-img">
 			<div class="info f_left">
 				<div class="names">
 					<span class="qualification f_left">N项认证</span>
-					<span class="name f_left">商户主页</span>
+					<span class="name f_left">{{us.Name}}</span>
 				</div>
 				<div class="clear"></div>
-				<div class="grade">接单数:45/电话数:99/评分:5</div>
+				<div class="grade">接单数:{{us.OrderCount2}}&nbsp;/&nbsp;电话数:{{us.PhoneCount2}}&nbsp;/&nbsp;评分:{{us.Grade2}}</div>
 				<div class="servs">
-					<div class="ser-itel">充氟利昂</div>
-					<div class="ser-itel">充氟利昂</div>
-					<div class="ser-itel">小时工</div>
+					<p class="ser-itel" v-if="us.TagList.Count > 0" v-for="item in us.TagList.TagList">{{item.TagName}}</p>
 				</div>
 			</div>
-			<a href="tel:13148366695"><img src="../../static/images/call.png" alt="" class="img-call f_right"></a>
+			<a :href="'tel:'+us.PhoneNumber"><img src="../../static/images/call.png" alt="" class="img-call f_right"></a>
 			<div class="clear"></div>
-			<div class="activity-list">
+			<div class="activity-list" v-if="serviceTypeRules.length > 0">
 				<div class="items">
-					<div class="ac-item"><span class="ac-icon bg-o">减</span>在线支付满200减20</div>
-					<div class="ac-item"><span class="ac-icon bg-r">返</span>领取优惠券，满200减20</div>
+					<div class="ac-item" v-for="item in serviceTypeRules">
+						<span class="ac-icon bg-o" v-if="item.ReturnType === '0'">减</span>
+						<span class="ac-icon bg-r" v-if="item.ReturnType === '1'">返</span>
+						<span>{{item.Help}}</span>
+					</div>	
 				</div>
-				<div class="ac-nums f_right">2个活动</div>
+				<div class="ac-nums f_right">{{serviceTypeRules.length}}个活动</div>
 			</div>
 			<div class="clear"></div>
 		</div>
 		<div class="clear"></div>
 	</header>
-	<div class="main">
+	<div class="main" style="padding-top:160px;">
 		<div class="tab">
 			<ul class="score">
-				<li :class="{'active':flag==1}" @click="tab='aa',flag=1">服务</li>
-				<li :class="{'active':flag==2}" @click="tab='bb',flag=2">评价</li>
-				<li :class="{'active':flag==3}" @click="tab='cc',flag=3">商户</li>		
+				<li :class="{'active':tab===0}" @click="chooseTab(0)">服务</li>
+				<li :class="{'active':tab===1}" @click="chooseTab(1)">评价</li>
+				<li :class="{'active':tab===2}" @click="chooseTab(2)">商户</li>	
 			</ul>
 			<div class="clear"></div>
 		</div>
 		<!-- 服务tab -->
-		<div v-show="tab==='aa'" id="tab-fuwu">
-			<div class="type-list f_left">
+		<div v-show="tab===0" v-if="svList" style="margin-top:45px;height:calc(100vh - 205px);">
+			<div class="type-list f_left" >
 				<ul>
-					<li class="score" :class="{'ty-ac':Htab==1}" @click="Htab=1">最新优惠</li>
-					<li class="score" :class="{'ty-ac':Htab==2}" @click="Htab=2">小食甜品</li>
-					<li class="score" :class="{'ty-ac':Htab==3}" @click="Htab=3">金针菇</li>
-					<li class="score" :class="{'ty-ac':Htab==4}" @click="Htab=4">金针菇</li>
-					<li class="score" :class="{'ty-ac':Htab==5}" @click="Htab=5">金针菇</li>
-					<li class="score" :class="{'ty-ac':Htab==6}" @click="Htab=6">金针菇</li>
-					<li class="score" :class="{'ty-ac':Htab==7}" @click="Htab=7">金针菇</li>
-					<li class="score" :class="{'ty-ac':Htab==8}" @click="Htab=8">aaaaa</li>
+					<li class="score" v-for="(item,index) in svList" :class="{'ty-ac':Htab==index}" @click="Htab=index">
+					<a style="display:block;padding: 15px 5px 15px 5px;color:#333" href="javascript:;">{{item.Name}}</a></li>
 				</ul>
 			</div>
-			<div class="type-cont f_right">
-				<div class="type-nums">一个分类</div>
+			<div class="type-cont f_right" v-for="items in svList" id="">
+				<div class="type-nums">{{items.ChildCount}}个分类</div>
 				<div class="cont-lists">
-					<div class="cont-item score">
-						<div class="cont-name">木工</div>
-						<div class="cont-price"><span class="price">￥250</span>元起</div>
-						<div class="btn-buy">购买</div>
-					</div>
-					<div class="cont-item score">
-						<div class="cont-name">小时工</div>
-						<div class="cont-price"><span class="price">￥250</span>元起</div>
-						<div class="btn-buy">购买</div>
-					</div>
-					<div class="cont-item score">
-						<div class="cont-name">金针工</div>
-						<div class="cont-price"><span class="price">￥250</span>元起</div>
-						<div class="btn-buy">购买</div>
+					<div class="cont-item score" v-for="item in items.Child">
+						<div class="cont-name">{{item.Name}}</div>
+						<div class="cont-price"><span class="price">￥{{item.Price}}</span></div>
+						<div class="btn-buy" @click="routerToOrder()">购买</div>
 					</div>
 				</div>
-
 			</div>
 			<div class="clear"></div>
 		</div>
 		<!-- 评价tab -->
-		<div v-show="tab==='bb'">
+		<div v-show="tab===1" style="margin-top:45px;height:calc(100vh - 205px);">
 			<div class="pingjia-list">
 				<!-- 没有评价要显示的 -->
 				<div class="no-pingjia">
@@ -85,7 +69,7 @@
 					<div class="no-title">暂无评价</div>
 				</div>
 				<!-- 有评价要显示的 -->
-				<div class="pingjia-item score">
+				<!-- <div class="pingjia-item score">
 					<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="" class="img-pingjia f_left">
 					<div class="pingjia-info f_left">
 						<div class="phone">
@@ -106,8 +90,8 @@
 						</div>
 					</div>
 					<div class="clear"></div>
-				</div>
-				<div class="pingjia-item score">
+				</div> -->
+				<!-- <div class="pingjia-item score">
 					<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="" class="img-pingjia f_left">
 					<div class="pingjia-info f_left">
 						<div class="phone">
@@ -128,74 +112,59 @@
 						</div>
 					</div>
 					<div class="clear"></div>
-				</div>
+				</div> -->
 			</div>
-			<img src="../../static/images/write-cont.png" alt="" class="write-cont">
+			<!-- <img src="../../static/images/write-cont.png" alt="" class="write-cont"> -->
 		</div>
 		<!-- 商户tab -->
-		<div v-show="tab==='cc'">
+		<div v-show="tab===2" style="margin-top:45px;height:calc(100vh - 205px);">
 			<div class="detail-info">
 				<div class="line"></div>
 				<div class="info-list">
 					<div class="info-title score">服务信息</div>
-					<div class="info-cont score">服务时间：10:00-12:00</div>
-					<div class="info-cont score">商户地址：延安里延安里延安里延安里里延安里</div>
-					<div class="info-cont score">服务范围：小时工 月嫂</div>
-					<div class="info-cont score">标签：
+					<div class="info-cont score">服务时间：{{us.ServiceTime}}</div>
+					<div class="info-cont score">商户地址：{{us.Address}}</div>
+					<div class="info-cont score">服务范围：{{us.ServiceScope}}</div>
+					<div class="info-cont score" v-if="us.TagList.Count > 0">标签：
 						<div class="info-tags f_right">
-							<span class="info-tag">人美</span>
-							<span class="info-tag">善良</span>
-							<span class="info-tag">金针菇</span>
-							<span class="info-tag">金针菇</span>
-							<span class="info-tag">金针菇</span>
-							<span class="info-tag">金针菇</span>
-							<span class="info-tag">金针菇</span>
-							<span class="info-tag">金针</span>
+							<span class="info-tag" v-for="item in us.TagList.TagList">{{item}}</span>
 						</div>
 						<div class="clear"></div>
 					</div>
 					<div class="info-cont">认证：
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="" class="img-rz">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="" class="img-rz">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="" class="img-rz">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="" class="img-rz">
+						<img v-for="item in us.SystemCertification" :src="item.Image" alt="" class="img-rz">
 					</div>
 				</div>
 				<div class="line"></div>
 				<div class="info-list">
 					<div class="info-title score">商户信息</div>
 					<div class="worker-info f_left score-right">	
-						<div class="worker-cont">成立时间：2016.1.14</div>
-						<div class="worker-cont">营业面积：100cm</div>
-						<div class="worker-cont">经营规模：30万元</div>
+						<div class="worker-cont">成立时间：{{us.EstablishedTime}}</div>
+						<div class="worker-cont">营业面积：{{us.Area}}</div>
+						<div class="worker-cont">经营规模：{{us.Scale}}</div>
 					</div>
 					<div class="worker-info f_right">
-						<div class="worker-cont">商户性质：个体</div>
-						<div class="worker-cont">员工人数：5人</div>
+						<div class="worker-cont">商户性质：{{us.Property}}</div>
+						<div class="worker-cont">员工人数：{{us.StaffNumber}}</div>
 					</div>
 					<div class="clear"></div>
 					<div class="info-sign">
-						<div class="signature score">个性签名：我是帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥</div>
-						<div class="signature">系统介绍：我是帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥帅哥</div>
+						<div class="signature score">个性签名：{{us.Signature}}</div>
+						<div class="signature">系统介绍：{{us.Intro}}</div>
 					</div>
 				</div>
 				<div class="line"></div>
 				<div class="info-list">
 					<div class="info-title score">商户照片及员工服务照</div>
 					<div class="worker-img">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="">
-						<img src="http://up.qqya.com/allimg/201710-t/17-101803_106599.jpg" alt="">
+						<img v-for="item in avList" :src="item" alt="">
 					</div>
 				</div>
 				<div class="line"></div>
 				<div class="info-list">
 					<div class="info-title score">营业执照</div>
 					<div class="licence-img">
-						<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487741338921&di=112a961418a1d1b4d16edf086ea797a8&imgtype=0&src=http%3A%2F%2Fimg.jdzj.com%2FUserDocument%2F2014c%2Fgym2014%2FPicture%2F2014528102617.jpg" alt="">
-						<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487741338921&di=112a961418a1d1b4d16edf086ea797a8&imgtype=0&src=http%3A%2F%2Fimg.jdzj.com%2FUserDocument%2F2014c%2Fgym2014%2FPicture%2F2014528102617.jpg" alt="">
+						<img v-for="item in us.LicencePhotos" :src="item" alt="">
 					</div>
 				</div>				
 			</div>
@@ -218,6 +187,7 @@
 
 <script>
 import { mapState } from 'vuex';
+import BScroll from 'better-scroll';
 import InfiniteLoading from 'vue-infinite-loading';
 import API from '../../config/backend';
 import axios from 'axios';
@@ -227,17 +197,152 @@ export default {
 	name:"business",
   data(){
     return{
-        tab:'aa',
-        flag:1,
-        Htab:1
+      tab: 0,
+      Htab: 0,
+      us:{		// 商户详情
+      	TagList:{
+      		TagList:[]
+      	}
+      },
+      ac:null,	// 商户评价列表
+      avList:null, // 商户头像列表
+      scroll:null,	// 滚动对象挂载
+      svList:null,// 商户服务列表 
+      isError:false,
+      errorMsg:""
     }            
   },
   mounted(){
-  	if(this.objectInfo){
+  	if(this.objectInfo.Id){
+  		axios.post(API.DetailEx,qs.stringify({
+	      "Token": this.Token,
+	      "Type": this.objectInfo.Type,
+	      "Id": this.objectInfo.Id
+	    }),{
+	      headers: {'Content-Type':'application/x-www-form-urlencoded'}
+	    }).then((res)=>{
+	      console.log("商户详情",res.data);
+	      if(res.data.Meta.ErrorCode === '0'){
+	        this.us =res.data.Body.Business;
+	      }else{
+	        this.isError = true;
+	        this.errorMsg = res.data.Meta.ErrorMsg;
+	      }
+	    }).catch(function (error) {
+	      console.log(error);
+	      this.isError = true;
+	      this.errorMsg = "小主，请在WIFI，4g环境下享用本服务 么么哒!";
+	    });
+	    this.getServiceList();    
+  	}
+  },
+  methods:{
+  	chooseTab(index){
+  		// 0 服务 1 评价 2 工人
+  		this.tab = index;
+  		switch (index) {
+  			case 0 :
+  				this.getServiceList();
+  				break;
+  			case 1 :
+  				this.getAssess();
+  				break;
+  			case 2 :
+  				this.getWorkerDetail();
+  				break;
+  			default :
+  				break;
+  		}
+  	},
+  	getServiceList(){
+  		window.scrollTo(0,0);
+			axios.post(API.GetMerchantServicePriceListEx,qs.stringify({
+		    "Token": this.Token,
+		    "Id": this.objectInfo.Id
+		  }),{
+		    headers: {'Content-Type':'application/x-www-form-urlencoded'}
+		  }).then((res)=>{
+		    console.log("商户服务列表",res.data);
+		    if(res.data.Meta.ErrorCode === '0'){
+		      this.svList = res.data.Body.ServiceTypeList;
+		      this.scroll = new BScroll(document.getElementById('wrapper0'), {
+					  startX: 0,
+					  startY: 0
+					})					
+		    }else{
+		      this.isError = true;
+		      this.errorMsg = res.data.Meta.ErrorMsg;
+		    }
+		  }).catch(function (error) {
+		    console.log(error);
+		    this.isError = true;
+		    this.errorMsg = "小主，请在WIFI，4g环境下享用本服务 么么哒!";
+		  });
+  	},
+  	getAssess(){
+  		window.scrollTo(0,0);
+  		// axios.post(API.GetWorkerEvaluationList,qs.stringify({
+		  //   "ID": this.objectInfo.Id,
+		  //   "PageIndex": "10",
+		  //   "PageSize": "1"
+		  // }),{
+		  //   headers: {'Content-Type':'application/x-www-form-urlencoded'}
+		  // }).then((res)=>{
+		  //   console.log("工人评价列表",res.data);
+		  //   if(res.data.Meta.ErrorCode === '0'){
+		  //     this.ac = res.data.Body.EvaluationList;
+		  //     this.scroll = new BScroll(document.getElementById('wrapper0'), {
+				// 	  startX: 0,
+				// 	  startY: 0
+				// 	})					
+		  //   }else{
+		  //     this.isError = true;
+		  //     this.errorMsg = res.data.Meta.ErrorMsg;
+		  //   }
+		  // }).catch(function (error) {
+		  //   console.log(error);
+		  //   this.isError = true;
+		  //   this.errorMsg = "小主，请在WIFI，4g环境下享用本服务 么么哒!";
+		  // });
+  	},
+  	getWorkerDetail(){
+  		window.scrollTo(0,0);
+  		axios.post(API.Avatar,qs.stringify({
+		    "Type": '2',
+		    "Id": this.objectInfo.Id
+		  }),{
+		    headers: {'Content-Type':'application/x-www-form-urlencoded'}
+		  }).then((res)=>{
+		    console.log("商户头像列表",res.data);
+		    if(res.data.Meta.ErrorCode === '0'){
+					this.avList = res.data.Body.Avatars;
+		    }else{
+		      this.isError = true;
+		      this.errorMsg = res.data.Meta.ErrorMsg;
+		    }
+		  }).catch(function (error) {
+		    console.log(error);
+		    this.isError = true;
+		    this.errorMsg = "小主，请在WIFI，4g环境下享用本服务 么么哒!";
+		  });
+  	},
+  	routerToOrder(){
   		
   	}
   },
   computed:{
+  	serviceTypeRules(){
+  		// 二维数组扁平化
+  		let list = [];
+  		if(this.us.hasOwnProperty('ActivityNgs')){
+  			this.us.ActivityNgs.ServiceTypeRules.map((v,i,arry)=>{
+	  			v.Details.map((x)=>{
+	  				list.push(x);
+	  			})
+	  		}) 		
+  		} 	
+  		return list;	
+  	},
   	...mapState(['Token','objectInfo'])
   }
 }
@@ -377,6 +482,13 @@ header{
 
 
 /*tab切换框*/
+.tab {
+	position: fixed;
+  background: #fff;
+  top: 160px;
+  width: 100%;
+  z-index:3;
+}
 .tab ul{
 	height: 45px;
 }
@@ -408,7 +520,7 @@ header{
     height: 100%;
 }
 .type-list li{
-    padding: 15px 5px 15px 5px;
+   /* padding: 15px 5px 15px 5px;*/
 }
 .ty-ac{
 	background: #fff;
@@ -416,8 +528,6 @@ header{
 }
 .type-cont{
 	width: 78.7%;
-	height: 100%;
-    overflow-y: scroll;
 }
 .type-nums{
 	background: #f8f8f8;
@@ -486,10 +596,9 @@ header{
 	margin-top: 10px;
 }
 .no-pingjia{
-	display: none;
 	text-align: center;
-    color: #989898;
-    margin-top: 15%;
+  color: #989898;
+	margin-top: 30%;
 }
 .no-pingjia img{
 	width: 30%;
